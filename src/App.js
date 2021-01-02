0a7569f4cb7fb7a0e2fc62e77528ef5f3fb04bd5
akanksha <akankshasaini03@gmail.com>
2021-01-02 19:02:57 +0530

    User input displays

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
 import logo from './logo.svg';
 import react from 'react';
+import ReactDOM from 'react-dom';
 import './App.css';
 
-function App() {
+class App extends react.Component {
+  constructor(){
+    super();
+    this.state = {
+      keyPressed : ""
+    }
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  componentDidMount() {
+    this.focusDiv();
+  }
+
+  componentDidUpdate() {
+    if(this.state.active)
+      this.focusDiv();
+  }
+
+  focusDiv() {
+    console.log("in focus");
+    ReactDOM.findDOMNode(this.refs.mainDiv).focus();
+  }
+
+  handleKeyPress(event){
+    this.setState ({
+      keyPressed : this.state.keyPressed + event.key,
+    });
+  }
+
+
+ render(){
   return (
     <>
-        <span>Welcome to typing expert</span>
-        <Keyboard keyPressed={2} />
+        <div ref="mainDiv" tabIndex="0" onKeyPress={this.handleKeyPress}> 
+          <h2>Welcome to typing expert</h2>
+          <span> Type these words </span>
+          <br/>
+          <span> there you go </span>
+          <br/>
+          <Keyboard keyPressed={this.state.keyPressed} />
+        </div>
     </>
   );
+  }
 }
 
 
@@ -22,8 +60,7 @@ class Keyboard extends react.Component {
   }
 
   HeighlightKey(key){
-    const keys = this.state.keys;
-    keys[key] = true
+    const keys = this.state.keys.slice();
     this.state = {
       keys : keys,
       heighlighted_key : key
